1ccf0ee2670d5d962065e8d9e97ab163015fcf59
suriya shanmugam <suriya.shanmugam.us@gmail.com>
2026-03-13 16:04:46 -0700

    python lab

--- python_essentials/lab.py
+++ python_essentials/lab.py
@@ -1,8 +1,24 @@
+'''Input: nums = [100,4,200,1,3,2]
+Output: 4'''
+nums = []
+nums_sorted = sorted(nums)
+#[1,2,3,100,200]
 
-l = [1,2,3]
 
-for i in range(len(l)):
-    for j in range(i,len(l)) :
-        if j == 1 :
-            break
-    print(j)
+# max_count = -1
+# count = 1 
+# for i in range(1,len(nums)):
+#     if nums[i] - nums[i-1] ==  1 :
+#         count += 1
+#     else :
+#         count = 1
+#     max_count = max(count,max_count)
+
+count = 1 
+visited = set(nums)
+for num in nums :
+    if num-1 in visited :
+        count +=1
+    else :
+        count = 1 
+    max_count = max(count,max_count)
